d4a0916de90bb1cd95201b651678c94a6ff0b1e6
thanicha <110970224+ThanichaMongkorn@users.noreply.github.com>
2023-01-10 16:14:57 +0700

    labbb13.1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-int fibonacci(int x);
+long long int fibonacci(int x);
     
 int main(){
     int x;
@@ -10,11 +10,11 @@ int main(){
     return 0; 
 }
 
-int fibonacci(int x){
+long long int fibonacci(int x){
     if(x>=0 and x<=1){
         return x; 
     }else{
-        return x=fibonacci(x-1)+fibonacci(x-2);
+        return fibonacci(x-1)+fibonacci(x-2);
         
 
     }
